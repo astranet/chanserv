b5d6b19bb4ec28abb4096ff3f43778b7c62fb94e
Maxim Kupriianov <max@kc.vc>
2019-04-09 12:49:44 +0300

    Upd.

--- benchkit_test.go
+++ benchkit_test.go
@@ -9,8 +9,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/astranet/astranet"
 	"github.com/rcrowley/go-metrics"
-	"github.com/zenhotels/astranet"
 )
 
 var chanservCli Client
--- chanserv_test.go
+++ chanserv_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/zenhotels/astranet"
+	"github.com/astranet/astranet"
 )
 
 func init() {
